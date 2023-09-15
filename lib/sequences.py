c5a65ddfa5a62de512367ab9780313e6fa10bba6
Cristina Lynn <cristina@cristinalynn.net>
2023-09-15 16:38:40 -0700

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    for n in range(length):
+        if n == 0:
+            fibonacci_sequence.append(n)
+
+        if n == 1:
+            fibonacci_sequence.append(n)
+
+        if n > 1:
+            sum = fibonacci_sequence[-1] + fibonacci_sequence[-2]  
+            fibonacci_sequence.append(sum)
+
+    print(fibonacci_sequence)          
